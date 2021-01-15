60fbd0d2bcb4689c64d5299a028dffd56941eb40
CurtisHorton <curtis.h.horton@gmail.com>
2021-01-15 10:58:47 -0330

    Initial Commit, stuck in loop

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.wifi.WifiNetworkSpecifier
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun onConnectClick (view: View)
+    {
+        val wifiSpecs = WifiNetworkSpecifier.Builder()
+            .setSsid("RoboBar_v2")
+            .setWpa2Passphrase("")
+            .build()
+        val networkRequest = NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .setNetworkSpecifier(wifiSpecs)
+            .build()
+        val connectivityManager = getSystemService((Context.CONNECTIVITY_SERVICE)) as ConnectivityManager
+        val networkCallback = object: ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                connectivityManager.bindProcessToNetwork(network)
+            }
+        }
+
+        connectivityManager.requestNetwork(networkRequest, networkCallback)
+        Reply.text = "Connect Clicked"
+
+    }
+}
